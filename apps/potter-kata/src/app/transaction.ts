26e02fa5af385e59176d9a5f441a9b9c56f026fb
hsinlunlun <hsinlun.chiu@gmail.com>
2022-05-15 23:31:55 +0800

    test case 2: green light

--- apps/potter-kata/src/app/transaction.ts
+++ apps/potter-kata/src/app/transaction.ts
@@ -1,23 +1,61 @@
 export class Transaction {
   price(books: number[]){
     let bill = 0;
+    let seriesCount = [0, 0, 0, 0, 0]; // 0, 1, 2, 3, 4目前各0本
 
-    
     books.forEach(book => {
       switch (book) {
         case 0:
+          seriesCount[0]++;
+          break;
         case 1:
+          seriesCount[1]++;
+          break;
         case 2:
+          seriesCount[2]++;
+          break;
         case 3:
+          seriesCount[3]++;
+          break;
         case 4:
-          bill += 8;
+          seriesCount[4]++;
           break;
         default:
-          bill += 0;
           break;
       }
     });
 
+    let isDiscountable = 0;
+
+    seriesCount.forEach(episode => {
+      if (episode !== 0) {
+        isDiscountable++;
+      }
+    });
+
+    if (isDiscountable >= 2) { // 有兩本以上不同的書
+      switch (isDiscountable) {
+        case 2:
+          bill += 8*2*0.95
+          break;
+        case 3:
+          bill += 8*3*0.9
+          break;
+        case 4:
+          bill += 8*4*0.8
+          break;
+        case 5:
+          bill += 8*5*0.75
+          break;
+      
+        default:
+          break;
+      }
+    }else{
+      let copiesOfAEpisode = Math.max(...seriesCount);
+      bill = 8*copiesOfAEpisode;
+    }
+      
     return bill;
   }
 }
