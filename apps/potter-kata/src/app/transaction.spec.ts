2ca1bbb51bb84a2cc6afeba3b61280b3e66aae25
hsinlunlun <hsinlun.chiu@gmail.com>
2022-05-15 22:58:52 +0800

    Test case 1 modified

--- apps/potter-kata/src/app/transaction.spec.ts
+++ apps/potter-kata/src/app/transaction.spec.ts
@@ -7,6 +7,7 @@ describe('Transaction', () => {
 
   // testcase1: no discount
   test('3 copies of 2st book', () => {
+    const transaction = new Transaction();
     expect(transaction.price([1])).toBe(8*3);
   });
 });
