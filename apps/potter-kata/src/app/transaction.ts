670070283531917b0aea0acea94cc6390762e891
hsinlunlun <hsinlun.chiu@gmail.com>
2022-05-16 00:32:32 +0800

    Test case 3: green light + 修正打錯的測試資料算式

--- apps/potter-kata/src/app/transaction.ts
+++ apps/potter-kata/src/app/transaction.ts
@@ -25,37 +25,54 @@ export class Transaction {
       }
     });
 
-    let isDiscountable = 0;
+    while (Math.max(...seriesCount) !== 0) {
+      let isDiscountable = 0;
 
-    seriesCount.forEach(episode => {
-      if (episode !== 0) {
-        isDiscountable++;
-      }
-    });
+      // 現在最大可使用的折扣
+      seriesCount.forEach(episode => {
+        if (episode !== 0) {
+          isDiscountable++;
+        }
+      });
+      console.log(seriesCount);
+      console.log(isDiscountable)
 
-    if (isDiscountable >= 2) { // 有兩本以上不同的書
-      switch (isDiscountable) {
-        case 2:
-          bill += 8*2*0.95
-          break;
-        case 3:
-          bill += 8*3*0.9
-          break;
-        case 4:
-          bill += 8*4*0.8
-          break;
-        case 5:
-          bill += 8*5*0.75
-          break;
-      
-        default:
-          break;
+      if (isDiscountable >= 2) { // 可以使用折扣(有兩本以上不同的書)
+        switch (isDiscountable) {
+          case 2:
+            bill += 8*2*0.95
+            break;
+          case 3:
+            bill += 8*3*0.9
+            break;
+          case 4:
+            bill += 8*4*0.8
+            break;
+          case 5:
+            bill += 8*5*0.75
+            break;
+          default:
+            break;
+        }
+        for (let index = 0; index < seriesCount.length; index++) {// 扣除此次使用了折扣的書
+          if (seriesCount[index] !== 0) {
+            seriesCount[index]--;
+          } 
+        }
+
+      }else{ // 只買了一集
+        let copiesOfAEpisode = 0;
+        for (let index = 0; index < seriesCount.length; index++) {// 找出買了幾本，然後扣掉
+          if (seriesCount[index] > copiesOfAEpisode) {
+            copiesOfAEpisode = seriesCount[index];
+            bill += 8*copiesOfAEpisode;
+
+            seriesCount[index] = 0;
+          } 
+        }
       }
-    }else{
-      let copiesOfAEpisode = Math.max(...seriesCount);
-      bill += 8*copiesOfAEpisode;
-    }
-      
+    }// end white
+
     return bill;
   }
 }
