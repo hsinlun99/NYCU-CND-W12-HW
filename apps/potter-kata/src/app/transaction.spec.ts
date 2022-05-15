beb361ec93a9866dbe579863b15302a5f28f3b4f
hsinlunlun <hsinlun.chiu@gmail.com>
2022-05-15 22:57:04 +0800

    Test case 1

--- apps/potter-kata/src/app/transaction.spec.ts
+++ apps/potter-kata/src/app/transaction.spec.ts
@@ -4,4 +4,9 @@ describe('Transaction', () => {
   it('should create an instance', () => {
     expect(new Transaction()).toBeTruthy();
   });
+
+  // testcase1: no discount
+  test('3 copies of 2st book', () => {
+    expect(transaction.price([1])).toBe(8*3);
+  });
 });
