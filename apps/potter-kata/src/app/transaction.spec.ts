65d1285cbbd5a993b21c8060d8461f7f3f0d9bbb
hsinlunlun <hsinlun.chiu@gmail.com>
2022-05-15 23:36:12 +0800

    Refactor 1

--- apps/potter-kata/src/app/transaction.spec.ts
+++ apps/potter-kata/src/app/transaction.spec.ts
@@ -1,19 +1,23 @@
 import { Transaction } from './transaction';
 
 describe('Transaction', () => {
+  let transaction: Transaction;
+
+  beforeEach(() => {
+    transaction = new Transaction();
+  });
+
   it('should create an instance', () => {
-    expect(new Transaction()).toBeTruthy();
+    expect(transaction).toBeTruthy();
   });
 
   // testcase1: no discount
   test('3 copies of 2st book', () => {
-    const transaction = new Transaction();
     expect(transaction.price([1, 1, 1])).toBe(8*3);
   });
 
   // testcase2: simple discount 
   test('2 different books', () => {
-    const transaction = new Transaction();
     expect(
       transaction.price([0, 1]) + transaction.price([0, 2, 4]) + transaction.price([0, 1, 2, 4]) + transaction.price([0, 1, 2, 3, 4])
       ).toBe(8*2*0.95 + 8*3*0.9 + 8*4*0.8 + 8*5*0.75);
