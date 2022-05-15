9c0a5ad6e7ef069339df9850a2b7b3b4e04cf165
hsinlunlun <hsinlun.chiu@gmail.com>
2022-05-16 00:39:34 +0800

    test case 4: edge cases

--- apps/potter-kata/src/app/transaction.spec.ts
+++ apps/potter-kata/src/app/transaction.spec.ts
@@ -45,4 +45,20 @@ describe('Transaction', () => {
         (8 + 8*5*0.75)
       );
   });
+
+  // testcase 4: edge cases
+  test('edge cases', () => {
+    expect(
+        transaction.price([0, 0, 1, 1, 2, 2, 3, 4]) + 
+        transaction.price([
+          0, 0, 0, 0, 0, 
+          1, 1, 1, 1, 1, 
+          2, 2, 2, 2, 
+          3, 3, 3, 3, 3, 
+          4, 4, 4, 4]) 
+      ).toBe(
+        (2 * (8 * 4 * 0.8)) + 
+        (3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8)) 
+      );
+  });
 });
