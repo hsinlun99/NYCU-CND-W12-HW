766ae0a285206cc26f27ec1382857f4c32c140f5
hsinlunlun <hsinlun.chiu@gmail.com>
2022-05-15 23:02:00 +0800

    Test Case 1 Green light

--- apps/potter-kata/src/app/transaction.ts
+++ apps/potter-kata/src/app/transaction.ts
@@ -1,3 +1,21 @@
 export class Transaction {
-  
+  price(books: number[]){
+    let bill = 0;
+    books.forEach(book => {
+      switch (book) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+          bill += 8;
+          break;
+        default:
+          bill += 0;
+          break;
+      }
+    });
+
+    return bill;
+  }
 }
